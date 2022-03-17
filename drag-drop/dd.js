da9a484785c72b0fa109ed8f41a4f897d696e7d6
crisleo94 <cristhian.reinoso@live.com>
2022-03-17 01:09:09 -0500

    install http server, create drag and drop using vanilla HTML/JS

---
+++ drag-drop/dd.js
@@ -0,0 +1,46 @@
+const items = document.querySelectorAll('.item')
+const columns = document.querySelectorAll('.column')
+
+let dragItem = null
+
+items.forEach((item) => {
+  item.addEventListener('dragstart', dragStart)
+  item.addEventListener('dragend', dragEnd)
+})
+
+function dragStart() {
+  console.log('drag started')
+  dragItem = this
+  setTimeout(() => (this.className = 'invisible'), 0)
+}
+
+function dragEnd() {
+  console.log('drag ended')
+  this.className = 'item'
+  dragItem = null
+}
+
+columns.forEach((column) => {
+  column.addEventListener('dragover', dragOver)
+  column.addEventListener('dragenter', dragEnter)
+  column.addEventListener('dragleave', dragLeave)
+  column.addEventListener('drop', dragDrop)
+})
+
+function dragOver(e) {
+  e.preventDefault()
+  console.log('drag over')
+}
+
+function dragEnter() {
+  console.log('drag entered')
+}
+
+function dragLeave() {
+  console.log('drag left')
+}
+
+function dragDrop() {
+  console.log('drag dropped')
+  this.append(dragItem)
+}
